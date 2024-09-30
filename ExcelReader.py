d4e3b00348fc573976004d0878459d9c712291d1
Cesar Salva <cesarsalva91@gmail.com>
2024-09-30 15:29:16 -0300

    clase ExcelReader optimizada y funcional

--- ExcelReader.py
+++ ExcelReader.py
@@ -1,39 +1,27 @@
-import openpyxl
+import pandas as pd
 from typing import List, Dict
 
 class ExcelReader:
-    def __init__(self, file_path: str, sheet_name: str = None):
+    def __init__(self, file_path: str):
         self.file_path = file_path
-        self.sheet_name = sheet_name
-        self.workbook = None
-        self.sheet = None
-
-    def load_workbook(self):
-        """Carga el archivo Excel."""
-        try:
-            self.workbook = openpyxl.load_workbook(self.file_path)
-            if self.sheet_name:
-                self.sheet = self.workbook[self.sheet_name]
-            else:
-                self.sheet = self.workbook.active
-        except Exception as e:
-            print(f"Error al cargar el archivo Excel: {e}")
-            raise
+        self.data = None
 
     def read_equipment_data(self) -> List[Dict[str, str]]:
         """Lee los datos de los equipos desde la hoja de cálculo."""
-        if not self.sheet:
-            self.load_workbook()
-
-        equipment_data = []
-        headers = [cell.value for cell in self.sheet[1]]
-
-        for row in self.sheet.iter_rows(min_row=2, values_only=True):
-            equipment = dict(zip(headers, row))
-            if all(equipment.values()):  # Asegurarse de que todas las celdas tengan valor
-                equipment_data.append(equipment)
-
-        return equipment_data
+        try:
+            # Lee el archivo Excel directamente en un DataFrame de pandas
+            self.data = pd.read_excel(self.file_path)
+            
+            # Convierte el DataFrame a una lista de diccionarios
+            equipment_data = self.data.to_dict('records')
+            
+            # Filtra las filas que tienen todos los valores
+            equipment_data = [eq for eq in equipment_data if all(eq.values())]
+            
+            return equipment_data
+        except Exception as e:
+            print(f"Error al leer el archivo Excel: {e}")
+            return []
 
     def print_equipment_data(self, data: List[Dict[str, str]]):
         """Imprime los datos de los equipos de forma legible."""
