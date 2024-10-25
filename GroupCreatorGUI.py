6f37aef343d2f1944dd464cac0aea60dc27d550f
Cesar Salva <cesarsalva91@gmail.com>
2024-10-25 11:34:51 -0300

    se agrega campo para agregar skin

--- GroupCreatorGUI.py
+++ GroupCreatorGUI.py
@@ -20,8 +20,13 @@ class GroupCreatorGUI:
         self.group_name_entry = ttk.Entry(self.root)
         self.group_name_entry.grid(row=0, column=1, pady=5)
 
+        # Campo para el skin del grupo
+        ttk.Label(self.root, text="Skin del Grupo:").grid(row=1, column=0, pady=5)
+        self.skin_entry = ttk.Entry(self.root)  # Nuevo campo de texto para el skin
+        self.skin_entry.grid(row=1, column=1, pady=5)
+
         # Checkboxes para los atributos
-        ttk.Label(self.root, text="Atributos:").grid(row=1, column=0, pady=5, columnspan=2)
+        ttk.Label(self.root, text="Atributos:").grid(row=2, column=0, pady=5, columnspan=2)
         self.attributes = [
             "read", "write", "test", "web", "local", "telnet", "ssh", "ftp",
             "reboot", "policy", "winbox", "password", "sniff", "sensitive",
@@ -32,19 +37,21 @@ class GroupCreatorGUI:
             var = tk.BooleanVar()
             cb = ttk.Checkbutton(self.root, text=attr, variable=var)
             # Distribuir en dos columnas
-            cb.grid(row=(index // 2) + 2, column=index % 2, sticky='w', padx=20)
+            cb.grid(row=(index // 2) + 3, column=index % 2, sticky='w', padx=20)
             self.checkboxes[attr] = var
 
         # Botón para crear el grupo
-        ttk.Button(self.root, text="Crear Grupo", command=self.create_group).grid(row=len(self.attributes) // 2 + 2, columnspan=2, pady=20)
+        ttk.Button(self.root, text="Crear Grupo", command=self.create_group).grid(row=len(self.attributes) // 2 + 3, columnspan=2, pady=20)
 
     def create_group(self):
         group_name = self.group_name_entry.get()
+        skin = self.skin_entry.get()  # Obtener el skin del nuevo campo de texto
         if not group_name:
             messagebox.showerror("Error", "Por favor, ingrese un nombre para el grupo.")
             return
 
         self.new_group.set_group_name(group_name)
+        self.new_group.set_attribute("skin", skin)  # Establecer el skin
 
         # Recopilar atributos seleccionados
         selected_attributes = [attr for attr, var in self.checkboxes.items() if var.get()]
@@ -55,6 +62,7 @@ class GroupCreatorGUI:
         if self.new_group.run():
             messagebox.showinfo("Éxito", f"Grupo '{group_name}' creado exitosamente.")
             self.group_name_entry.delete(0, tk.END)  # Limpiar el campo de nombre
+            self.skin_entry.delete(0, tk.END)  # Limpiar el campo de skin
             for var in self.checkboxes.values():
                 var.set(False)  # Desmarcar todos los checkboxes
         else:
@@ -72,4 +80,3 @@ if __name__ == "__main__":
     router_manager = RouterManager("192.168.240.135", "admin", "admin", "8728")
     app = GroupCreatorGUI(router_manager)
     app.run()
-    
